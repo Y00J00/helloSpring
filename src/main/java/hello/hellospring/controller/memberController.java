b8e19180aaf2d8866e2dbb0942505cb6637cd2ed
Y00J00 <syeunnn@gmail.com>
2023-04-04 13:53:01 +0900

    2023-04-04 commit

---
+++ src/main/java/hello/hellospring/controller/memberController.java
@@ -0,0 +1,24 @@
+package hello.hellospring.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+import hello.hellospring.service.MemberService;
+
+@Controller
+public class memberController {
+
+    private final MemberService memberService;
+
+    @Autowired
+    public memberController(MemberService memberService) {
+        this.memberService = memberService;
+    }
+    //스프링 컨테이너가 뜰때 멤버컨트롤러 생성 -> 생성자 호출 
+    // -> @Autowired가 있는 생성자가 호출되었기 때문에 스프링 컨테이너에 있는 MemberService와 연결
+    // -> dependency injection
+
+
+    
+    
+}
