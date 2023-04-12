6f46e9023593035dbf6f25962869856dc024e55f
Y00J00 <syeunnn@gmail.com>
2023-04-12 13:33:13 +0900

    2023-04-12

--- src/main/java/hello/hellospring/controller/memberController.java
+++ src/main/java/hello/hellospring/controller/memberController.java
@@ -14,11 +14,7 @@ public class memberController {
     public memberController(MemberService memberService) {
         this.memberService = memberService;
     }
-    //스프링 컨테이너가 뜰때 멤버컨트롤러 생성 -> 생성자 호출 
-    // -> @Autowired가 있는 생성자가 호출되었기 때문에 스프링 컨테이너에 있는 MemberService와 연결
-    // -> dependency injection
-
-
-    
-    
+    //스프링 컨테이너가 뜰때 멤버컨트롤러 생성 -> 생성자 호출
+    // -> 생성자가  @Autowired가 있기 때문에 스프링 컨테이너에 있는 MemberService와 연결
+    // -> dependency injection    
 }
